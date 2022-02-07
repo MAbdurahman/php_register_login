fcbfb6c7e86c8a1af249987d16b1a211a51e557f
MAbdurahman <mdbdrrhm@gmail.com>
2022-02-07 11:03:05 -0600

    Add session_generated_id to app/controllers/Login.php

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -31,6 +31,8 @@
 
             if ($user) {
 
+                session_regenerate_id(true);
+
                 $_SESSION['user_id'] = $user->id;
 
                 $this->redirect('/');
@@ -43,4 +45,36 @@
             }
 
         }//end of the createAction Function
+
+        /**
+         * destroyAction Function - logs out a user
+         * @return void
+         */
+        public function destroyAction()
+        {
+            // Unset all of the session variables
+            $_SESSION = [];
+
+            // Delete the session cookie
+            if (ini_get('session.use_cookies')) {
+                $params = session_get_cookie_params();
+
+                setcookie(
+                    session_name(),
+                    '',
+                    time() - 42000,
+                    $params['path'],
+                    $params['domain'],
+                    $params['secure'],
+                    $params['httponly']
+                );
+            }
+
+            // Finally destroy the session
+            session_destroy();
+
+            $this->redirect('/');
+
+        }//end of the destroyAction Function
+
     }//end of the Login Class
