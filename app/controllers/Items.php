4d81afcfca96628a59dc6dd91e1fb355e8e60b82
MAbdurahman <mdbdrrhm@gmail.com>
2022-02-08 04:33:35 -0600

    Add app/Items.php and restricted access to only logged-in users

---
+++ app/controllers/Items.php
@@ -0,0 +1,28 @@
+<?php
+
+    namespace app\controllers;
+
+    use app\Auth;
+    use core\View;
+
+    /**
+     * Items Class
+     * PHP version 8.0.13
+     */
+    class Items extends \core\Controller
+    {
+
+        /**
+         * indexAction Function - displays an Items index view
+         * @return void
+         */
+        public function indexAction()
+        {
+            if (! Auth::isLoggedIn()) {
+                $this->redirect('/login');
+            }
+
+            View::renderTemplate('items/index.html');
+
+        }//end of the indexAction Function
+    }//end of the Items Class
