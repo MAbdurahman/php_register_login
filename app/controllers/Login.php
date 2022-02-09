17ef07dd0e00a7521af7553b16b629b25e369c0c
MAbdurahman <mdbdrrhm@gmail.com>
2022-02-08 18:20:26 -0600

    Add Flash message, when loggin out

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -5,6 +5,7 @@
     use app\Auth;
     use app\models\User;
     use core\View;
+    use app\Flash;
 
     /**
      * Login Class - is the Login Controller
@@ -33,11 +34,13 @@
             if ($user) {
 
                 Auth::login($user);
+                Flash::addMessage('Successful login');
                 $this->redirect(Auth::getReturnToPage());
 
 
             } else {
 
+                Flash::addMessage('Unsuccessful login, try again!');
                 View::renderTemplate('login/new.html', [
                     'email' => $_POST['email']
                 ]);
@@ -52,8 +55,23 @@
         public function destroyAction()
         {
             Auth::logout();
-            $this->redirect('/');
+            $this->redirect('/login/show-logout-message');
 
         }//end of the destroyAction Function
+
+        /**
+         * showLogoutMessageAction Function - shows a 'logged out' flash message and redirect to the
+         * homepage.  Necessary to use the flash messages as they use the session and at the end of
+         * logout method (destroyAction).  The session is destroyhed so a new action needs to called
+         * in order to use the session.
+         * @return void
+         */
+        public function showLogoutMessageAction()
+        {
+            Flash::addMessage('Logout successful');
+
+            $this->redirect('/');
+
+        }//end of the showLogoutMessageAction Function
 
     }//end of the Login Class
